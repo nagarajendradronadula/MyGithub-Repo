aa096a16371d73798bf98b77d63745a08b6a2ded
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2024-04-06 15:57:19 +0530

    started node js

---
+++ Apna Clg FSD Course/backend/math.js
@@ -0,0 +1,19 @@
+// const sum = (a, b) => a + b;
+module.exports.sum = (a, b) => a + b;
+// const mul = (a, b) => a * b;
+module.exports.mul = (a, b) => a * b;
+// const g = 9.8;
+module.exports.g = 9.8;
+// const PI = 3.14;
+module.exports.PI = 3.14;
+
+// module.exports = 123;
+
+// let obj = {
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// };
+// module.exports = obj;
