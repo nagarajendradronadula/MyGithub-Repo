4f4c9b8e9503fba10115a2062d4d976490ed1816
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2024-04-15 07:36:19 +0530

    completed express and RESTful APIs

---
+++ Apna Clg FSD Course/RESTful-APIs/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+const posts = [
+    {
+        id: uuidv4(),
+        username:"apnacollege",
+        content: "I Love Coding!"
+    },
+    {
+        id: uuidv4(),
+        username:"shardhakhapra",
+        content: "Hard Work is important to acheive success"
+    },
+    {
+        id: uuidv4(),
+        username:"dnr",
+        content: "Coding is my favourite subject"
+    }
+];
+
+app.get("/posts", (req, res) => {
+    // res.send("Server working well!");
+    res.render("index.ejs", {posts});
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let id = uuidv4();
+    // console.log(req.body);
+    let {username, content} = req.body;
+    posts.push({id, username, content})
+    // res.send("Post Request working");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    // console.log(id);
+    let post = posts.find((p) => id === p.id);
+    // console.log(post);
+    // res.send("request working");
+    res.render("show.ejs", {post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    // console.log(newContent);
+    post.content = newContent;
+    console.log(post)
+    // res.send("patch request working");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) => {
+    const {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    // res.send("Post Deleted");
+    res.redirect("/posts");
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
--- Apna Clg FSD Course/miscellaneous/frontend/app.js
+++ Apna Clg FSD Course/miscellaneous/frontend/app.js
@@ -1,8 +1,72 @@
-let arr = [1, 2, 3];
-let arr2 = [1, 2, 3];
-arr.sayHello = () => {
-    console.log("Hello!");
+// let arr = [1, 2, 3];
+// let arr2 = [1, 2, 3];
+// arr.sayHello = () => {
+//     console.log("Hello!");
+// }
+// arr2.sayHello = () => {
+//     console.log("Hello!");
+// }
+
+// function PersonMaker(name, age) {
+//     const person = {
+//         name: name,
+//         age: age,
+//         talk(){
+//             console.log(`Hi, my name is ${name}`);
+//         }
+//     };
+//     return person;
+// }
+
+// let p1 = PersonMaker("John", 20);
+// let p2 = PersonMaker("Jane", 21);
+
+// p1.talk() === p2.talk(); //false so this method is not a good way and this is called factory function
+
+//Constructors - doesn't return anyhing & start with capital
+// function Person(name, age) {
+//         this.name = name;
+//         this.age = age;
+// }
+// Person.prototype.talk = function () {
+//     console.log(`Hi, my name is ${this.name}`);
+// }
+
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     talk(){
+//         console.log(`Hi, my name is ${this.name}`);
+//     }
+// }
+
+// let p1 = new Person("John", 20);
+// let p2 = new Person("Jane", 21);
+
+// p1.talk === p2.talk; //true
+
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`Hi, my name is ${this.name}`);
+    }
 }
-arr2.sayHello = () => {
-    console.log("Hello!");
+
+class Student extends Person{
+    constructor(name, age, marks){
+        super(name, age); //parent class constructor is being called
+        this.marks = marks;
+    }
+}
+
+class Teacher extends Person{
+    consructor(name, age, subject){
+        super(name, age);
+        this.subject = subject;
+    }
 }
